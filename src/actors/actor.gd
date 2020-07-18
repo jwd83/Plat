4b418754598f4e222f0aaeb873d6488a9f6dcafc
Jared <jareddeblander@gmail.com>
2020-07-17 23:39:33 -0400

    Add files via upload

--- src/actors/actor.gd
+++ src/actors/actor.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 class_name actor
 
 
-export var speed: = Vector2(300,1000)
+export var speed: = Vector2(300.0,1000.0)
 export var gravity: = 3000.0
 
 var velocity: = Vector2.ZERO
@@ -10,6 +10,6 @@ var velocity: = Vector2.ZERO
 func _physics_process(delta):
 	#300 on xand 0 on y
 	velocity.y += gravity * delta
-	velocity.y = max(velocity.y, speed.y)
+	
 	velocity = move_and_slide(velocity)
 
